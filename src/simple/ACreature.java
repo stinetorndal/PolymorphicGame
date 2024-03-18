29dbcf2f7e1b23a48831ac4a83f4fa92d63e0a8e
Tess Gaston Løvgreen <tess@lovegreen.life>
2024-03-18 13:49:34 +0100

    abstract class added

---
+++ src/simple/ACreature.java
@@ -0,0 +1,33 @@
+package simple;
+
+abstract public class ACreature implements Creature{
+    protected String type;
+    protected int health;
+    protected String name;
+    public ACreature(){
+
+
+    }
+    public String getType(){
+        return this.type;
+    }
+
+    @Override
+    abstract public int attack();
+
+    @Override
+    abstract public String meet();
+
+    @Override
+    abstract public void takeDamage(int damage);
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+}
